2e4f7e5891e9bdc85482354c64495b7a90aac020
Enzo Costa <enzo_costa7@hotmail.com>
2025-04-14 15:02:18 -0300

    feat: get transactions data from api

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable, TransactionsWrapper } from "./styles";
+import { api } from "../../api";
+import { ITransaction } from "../../@types/transaction";
 
 export function Transactions() {
+    const [transactions, setTransactions] = useState<ITransaction[]>([])
+    
+    useEffect(() => {
+        loadTransactions()
+    }, [])
+
+    async function loadTransactions() {
+        const { data } = await api.get('/transactions')
+
+        setTransactions(data)
+    }
+
     return (
         <div>
             <Header />
@@ -14,26 +29,18 @@ export function Transactions() {
                     <TransactionsWrapper>
                         <TransactionsTable>
                             <tbody>
-                                <tr>
-                                    <td width="50%">Desenvolvimento de site</td>
-                                    <td>
-                                        <PriceHighlight variant="income">
-                                            R$ 12.000,00
-                                        </PriceHighlight>
-                                    </td>
-                                    <td>Venda</td>
-                                    <td>13/04/2022</td>
-                                </tr>
-                                <tr>
-                                    <td width="50%">Hamburguer</td>
-                                    <td>
-                                        <PriceHighlight variant="outcome">
-                                            - R$ 59,00
-                                        </PriceHighlight>
-                                    </td>
-                                    <td>Alimentação</td>
-                                    <td>10/04/2022</td>
-                                </tr>
+                                {transactions.map((transaction, index) => (
+                                    <tr key={index}>
+                                        <td width="50%">{transaction.description}</td>
+                                        <td>
+                                            <PriceHighlight variant={transaction.type}>
+                                                {transaction.price}
+                                            </PriceHighlight>
+                                        </td>
+                                        <td>{transaction.category}</td>
+                                        <td>{transaction.createdAt}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </TransactionsTable>
                     </TransactionsWrapper>
