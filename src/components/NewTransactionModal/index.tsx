68422bd86c7097bfb4c45caeb821bfe9c362717a
Enzo Costa <enzo_costa7@hotmail.com>
2025-04-14 13:04:52 -0300

    feat: create acessible radio button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -15,15 +15,15 @@ export function NewTransactionModal() {
 
                 <form action="">
                     <input type="text" placeholder="Descrição" required />
-                    <input type="text" placeholder="Preço" required />
+                    <input type="number" placeholder="Preço" required />
                     <input type="text" placeholder="Categoria" required />
 
                     <TransactionType>
-                        <TransactionTypeButton variant="income">
+                        <TransactionTypeButton value="income" variant="income">
                             <ArrowCircleUp size={24} />
                             Entrada
                         </TransactionTypeButton>
-                        <TransactionTypeButton variant="outcome">
+                        <TransactionTypeButton value="outcome" variant="outcome">
                             <ArrowCircleDown size={24} />
                             Saída
                         </TransactionTypeButton>
